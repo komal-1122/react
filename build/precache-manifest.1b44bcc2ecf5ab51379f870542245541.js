01995c1bb1c4152e4a9471b5c5f54f8b4114a7ad
komal-1122 <komalgulzarofficial@gmail.com>
2023-11-12 20:49:14 +0500

    changes

---
+++ build/precache-manifest.1b44bcc2ecf5ab51379f870542245541.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e900a7b05132431e05581193f1a8d5c2",
+    "url": "/react/index.html"
+  },
+  {
+    "revision": "6f840cde6eca2add4132",
+    "url": "/react/static/css/main.13bba0ff.chunk.css"
+  },
+  {
+    "revision": "6525bf8fc4385bf3c7a2",
+    "url": "/react/static/js/2.bcc28277.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/react/static/js/2.bcc28277.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eef90e41bcf13bdafb7b",
+    "url": "/react/static/js/3.82afb7a3.chunk.js"
+  },
+  {
+    "revision": "6f840cde6eca2add4132",
+    "url": "/react/static/js/main.d078a33f.chunk.js"
+  },
+  {
+    "revision": "c003ee4c3bf6098e5894",
+    "url": "/react/static/js/runtime-main.7e42a54e.js"
+  }
+]);
